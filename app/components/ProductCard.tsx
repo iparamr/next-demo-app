896c44ee69eca0c13a12a27d8a6a03d4ad54eceb
Param Randhawa <13896015+iparamr@users.noreply.github.com>
2024-08-05 10:39:18 +0530

    DaisyUI

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -7,7 +7,10 @@ const ProductCard = () => {
     // <div className={ styles.card }>
     //     <AddToCart />
     // </div>
-    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+    // <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
+    //     <AddToCart />
+    // </div>
+    <div>
         <AddToCart />
     </div>
   )
